ded4af1272163bb319a2d480df540628fb767c1d
Prateek Mahajan <prateekmahajan04@gmail.com>
2025-09-03 16:41:06 +0530

    code creview

--- parent/dev/main.tf
+++ parent/dev/main.tf
@@ -1,43 +1,43 @@
 module "RG" {
-  source         = "../../parent/azurerm_resource_group"
+  source         = "../../child/azurerm_resource_group"
   resource_group = var.resource_group
 
 
 }
 module "storage_account" {
-  source          = "../../parent/azurerm_storage_account"
+  source          = "../../child/azurerm_storage_account"
   storage_account = var.storage_account
 
   depends_on = [module.RG]
 }
 module "virtual_network" {
-  source = "../../parent/azurerm_virtual_network"
+  source = "../../child/azurerm_virtual_network"
   vnet   = var.vnet
 
   depends_on = [module.RG]
 }
 
 module "subnet" {
-  source     = "../../parent/azurerm_subnet"
-  subnet     = var.subn
-  depends_on = [module.RG, module.virtual_network]
+  source     = "../../child/azurerm_subnet"
+  subnet     = var.subnet
+  depends_on = [ module.virtual_network]
 }
 module "pip" {
-  source     = "../../parent/azurerm_public_ip"
+  source     = "../../child/azurerm_public_ip"
   pip        = var.pip
-  depends_on = [module.RG, module.virtual_network]
+  depends_on = [module.RG]
 }
 module "nic" {
-  source     = "../../parent/azurerm_nic"
+  source     = "../../child/azurerm_nic"
   nic        = var.nic
   subnet     = var.subnet
   vnet       = var.vnet
-  depends_on = [module.RG, module.virtual_network]
+  depends_on = [module.pip]
 }
 
 module "vm" {
-  source     = "../../parent/azurerm_virtual_machine"
+  source     = "../../child/azurerm_virtual_machine"
   vm         = var.vm
   nic        = var.nic
-  depends_on = [module.RG, module.virtual_network]
+  depends_on = [module.RG, module.nic, module.pip, module.virtual_network]
 }
